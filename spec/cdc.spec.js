0e0c69e625c3066bdfc277ba23163f84b894dbb1
Hermann Vocke <hvocke@thoughtworks.com>
2017-06-09 10:44:10 +0200

    implement first http test

--- spec/cdc.spec.js
+++ spec/cdc.spec.js
@@ -1,9 +1,14 @@
-describe("Player", function() {
+const request = require('request');
+
+describe("VNC Backend", function() {
 
   beforeEach(function() {
   });
 
-  it("should be able to play a Song", function() {
-    expect(true).toBe(false);
+  it("GET /catalog should return 200", function(done) {
+    request('https://httpbin.org/get', (error, response, body) => {
+	expect(response.statusCode).toBe(200);
+	done();
+    });
   });
 });
